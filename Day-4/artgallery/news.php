ad9a7f5ed08e5c3b974364680b00a88a442625e4
Akshat3721 <75060166+Akshat3721@users.noreply.github.com>
2021-05-30 15:21:30 +0530

    Add files via upload

---
+++ Day-4/artgallery/news.php
@@ -0,0 +1,80 @@
+
+<!doctype html>
+<!--
+	Photogenic by FreeHTML5.co
+	URL: https://freehtml5.co
+-->
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+        integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <!-- owl carousel css-->
+    <link rel="stylesheet" href="owl-carousel/assets/owl.carousel.min.css" type="text/css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Photogenic - Free Portfolio Bootstrap Template</title>
+</head>
+
+<body>
+ <?php include 'menu.php'; ?>
+<br/>
+<br/>
+<br/>
+<br/>
+
+    <div class="container-fluid fh5co-news" id="news">
+        <div class="container">
+            <h2>OUR NEWS</h2>
+            <div class="row">
+                <div class="owl-carousel owl-carousel2 owl-theme">
+                    <div>
+                        <div class="card text-center"> <img class="card-img-top" src="images/news1.png" alt="">
+                            <div class="card-body text-left pr-0 pl-0">
+                                <h5>News related to art gallery goes here. </h5>
+                                <p class="card-text">dsf </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="card text-center"> <img class="card-img-top" src="images/news2.png" alt="">
+                            <div class="card-body text-left pr-0 pl-0">
+                                <h5>News related to art gallery goes here. </h5>
+                                <p class="card-text">df</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="card text-center"> <img class="card-img-top" src="images/news3.png" alt="">
+                            <div class="card-body text-left pr-0 pl-0">
+                                <h5>News related to art gallery goes here. </h5>
+                                <p class="card-text">fd </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+   
+   <?php include 'footer.php'; ?>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="owl-carousel/owl.carousel.min.js"></script>
+    <script src="js/isotope-docs.min.js?6"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
